73ed4f8aa3ec663c0454c8ca30415e6b1df040e6
James Edward Gray II <james@graysoftinc.com>
2018-07-20 18:00:05 -0500

    Parsing models.

---
+++ nanobots/lib/nanobots/model.ex
@@ -0,0 +1,35 @@
+defmodule Nanobots.Model do
+  defstruct ~w[resolution matrix]a
+
+  def from_file(path) do
+    File.open!(path, ~w[read]a, &parse_model/1)
+  end
+
+  defp parse_model(device) do
+    <<resolution::integer>> = IO.binread(device, 1)
+    voxels = resolution |> :math.pow(3) |> trunc
+    bytes = voxels |> Kernel./(8) |> Float.ceil |> trunc
+    full_list =
+      device
+      |> IO.binread(bytes)
+      |> Stream.unfold(fn
+        <<full::size(1), rest::bitstring>> -> {full, rest}
+        "" -> nil
+      end)
+      |> Enum.chunk_every(8)
+      |> Enum.map(fn byte -> Enum.reverse(byte) end)
+      |> List.flatten
+      |> Enum.take(voxels)
+    matrix =
+      for x <- 0..(resolution - 1),
+          y <- 0..(resolution - 1),
+          z <- 0..(resolution - 1) do
+        {x, y, z}
+      end
+      |> Enum.zip(full_list)
+      |> Enum.filter(fn {_xyz, full} -> full == 1 end)
+      |> Enum.map(fn {xyz, _full} -> xyz end)
+      |> MapSet.new
+    %__MODULE__{resolution: resolution, matrix: matrix}
+  end
+end

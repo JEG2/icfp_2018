256f3a19a6a84b3513f67d7fd68b05bf293125ae
James Edward Gray II <james@graysoftinc.com>
2018-07-21 14:27:54 -0500

    Fixing traces.

--- nanobots/lib/nanobots/trace.ex
+++ nanobots/lib/nanobots/trace.ex
@@ -1,63 +1,71 @@
 defmodule Nanobots.Trace do
+  alias Nanobots.Commands.{
+    Halt, Wait, Flip, SMove, LMove, Fill, Fission, FusionP, FusionS
+  }
+
   defstruct ~w[device]a
 
   @moduledoc ~S"""
   alias Nanobots.Trace
+  alias Nanobots.Commands.{
+    Halt, Wait, Flip, SMove, LMove, Fill, Fission, FusionP, FusionS
+  }
 
   trace = Trace.new("test.nbt")
 
-  Trace.record_timestep(trace, [%{command: {:fission, {0, 0, 1}, 5}}])
-  Trace.record_timestep(trace, [%{command: {:fusion_p, {-1, 1, 0}}}])
-  Trace.record_timestep(trace, [%{command: {:fusion_s, {1, -1, 0}}}])
-  Trace.record_timestep(trace, [%{command: {:flip}}])
-  Trace.record_timestep(trace, [%{command: {:wait}}])
-  Trace.record_timestep(trace, [%{command: {:s_move, {12, 0, 0}}}])
-  Trace.record_timestep(trace, [%{command: {:s_move, {0, 0, -4}}}])
-  Trace.record_timestep(trace, [%{command: {:l_move, {3, 0, 0}, {0, -5, 0}}}])
-  Trace.record_timestep(trace, [%{command: {:l_move, {0, -2, 0}, {0, 0, 2}}}])
-  Trace.record_timestep(trace, [%{command: {:fill, {0, -1, 0}}}])
-  Trace.record_timestep(trace, [%{command: {:halt}}])
+  Trace.record_timestep(trace, [%Fission{nd: {0, 0, 1}, m: 5}])
+  Trace.record_timestep(trace, [%FusionP{nd: {-1, 1, 0}}])
+  Trace.record_timestep(trace, [%FusionS{nd: {1, -1, 0}}])
+  Trace.record_timestep(trace, [%Flip{ }])
+  Trace.record_timestep(trace, [%Wait{ }])
+  Trace.record_timestep(trace, [%SMove{lld: {12, 0, 0}}])
+  Trace.record_timestep(trace, [%SMove{lld: {0, 0, -4}}])
+  Trace.record_timestep(trace, [%LMove{sld1: {3, 0, 0}, sld2: {0, -5, 0}}])
+  Trace.record_timestep(trace, [%LMove{sld1: {0, -2, 0}, sld2: {0, 0, 2}}])
+  Trace.record_timestep(trace, [%Fill{nd: {0, -1, 0}}])
+  Trace.record_timestep(trace, [%Halt{ }])
 
   Trace.close(trace)
   """
 
   def new(path), do: %__MODULE__{device: File.open!(path, ~w[write]a)}
 
-  def record_timestep(%__MODULE__{device: device}, bots) when is_list(bots) do
-    Enum.each(bots, fn %{command: command} ->
+  def record_timestep(%__MODULE__{device: device}, commands)
+  when is_list(commands) do
+    Enum.each(commands, fn command ->
       encoded = encode_command(command)
       IO.binwrite(device, encoded)
     end)
   end
 
-  defp encode_command({:halt}), do: <<0b11111111>>
-  defp encode_command({:wait}), do: <<0b11111110>>
-  defp encode_command({:flip}), do: <<0b11111101>>
-  defp encode_command({:s_move, lld}) do
+  defp encode_command(%Halt{ }), do: <<0b11111111>>
+  defp encode_command(%Wait{ }), do: <<0b11111110>>
+  defp encode_command(%Flip{ }), do: <<0b11111101>>
+  defp encode_command(%SMove{lld: lld}) do
     {lld_a, lld_i} = ld_to_a_and_i(lld, 15)
     << 0b00::size(2), lld_a::size(2), 0b0100::size(4),
        0b000::size(3), lld_i::size(5) >>
   end
-  defp encode_command({:l_move, sld_1, sld_2}) do
-    {sld_1_a, sld_1_i} = ld_to_a_and_i(sld_1, 5)
-    {sld_2_a, sld_2_i} = ld_to_a_and_i(sld_2, 5)
-    << sld_2_a::size(2), sld_1_a::size(2), 0b1100::size(4),
-       sld_2_i::size(4), sld_1_i::size(4) >>
+  defp encode_command(%LMove{sld1: sld1, sld2: sld2}) do
+    {sld1_a, sld1_i} = ld_to_a_and_i(sld1, 5)
+    {sld2_a, sld2_i} = ld_to_a_and_i(sld2, 5)
+    << sld2_a::size(2), sld1_a::size(2), 0b1100::size(4),
+       sld2_i::size(4), sld1_i::size(4) >>
   end
-  defp encode_command({:fusion_p, dx_dy_dz}) do
+  defp encode_command(%FusionP{nd: dx_dy_dz}) do
     nd = dx_dy_dz_to_nd(dx_dy_dz)
     <<nd::size(5), 0b111::size(3)>>
   end
-  defp encode_command({:fusion_s, dx_dy_dz}) do
+  defp encode_command(%FusionS{nd: dx_dy_dz}) do
     nd = dx_dy_dz_to_nd(dx_dy_dz)
     <<nd::size(5), 0b110::size(3)>>
   end
-  defp encode_command({:fission, dx_dy_dz, m}) do
+  defp encode_command(%Fission{nd: dx_dy_dz, m: m}) do
     nd = dx_dy_dz_to_nd(dx_dy_dz)
     << nd::size(5), 0b101::size(3),
        m >>
   end
-  defp encode_command({:fill, dx_dy_dz}) do
+  defp encode_command(%Fill{nd: dx_dy_dz}) do
     nd = dx_dy_dz_to_nd(dx_dy_dz)
     <<nd::size(5), 0b011::size(3)>>
   end

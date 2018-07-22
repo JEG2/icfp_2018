d2e75c079ab37bf26c2590269dbd254b44742e91
claytonflesher <claytonflesher@gmail.com>
2018-07-22 12:13:27 -0500

    remove multi-threading

--- runner/app.rb
+++ runner/app.rb
@@ -6,23 +6,28 @@ problems = Dir.entries(File.expand_path("../problemsF", __dir__)).map do |file|
 end
 
 assemble_problems = problems.select { |name| name =~ /tgt\.mdl/ }
-chunked_assemblies = assemble_problems.each_slice(8)
+#chunked_assemblies = assemble_problems.each_slice(8)
 
 Dir.chdir(File.expand_path("../nanobots", __dir__)) do
   `mix escript.build`
 end
 
-chunked_assemblies.each do |group|
-  group.map do |location, acc|
-    Thread.new { puts %x(#{File.expand_path("../nanobots/nanobots", __dir__)} #{location}) }
-  end.each { |thread| thread.join }
+
+assemble_problems.each do |problem|
+  puts `#{File.expand_path("../nanobots/nanobots", __dir__)} #{problem}`
 end
 
-traces = Dir.entries(File.expand_path("../problemsF", __dir__)).map do |file|
-  File.expand_path("../problemsF/#{file}", __dir__)
-end.select { |file| file =~ /\.nbt/ }
+#chunked_assemblies.each do |group|
+#  group.map do |location, acc|
+#    Thread.new { puts system("#{File.expand_path("../nanobots/nanobots", __dir__)} #{location}") }
+#  end.each { |thread| thread.join }
+#end
 
 if SYSTEM_RUBY.contains('darwin')
+  traces = Dir.entries(File.expand_path("../problemsF", __dir__)).map do |file|
+    File.expand_path("../problemsF/#{file}", __dir__)
+  end.select { |file| file =~ /\.nbt/ }
+
   tmp_dir = Dir.mktmpdir('traces')
   FileUtils.cp(traces, tmp_dir)
   `zip -r traces.zip #{tmp_dir}`

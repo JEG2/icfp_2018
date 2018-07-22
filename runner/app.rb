f89a10852fc8abf83d5d34df20cfafd28e92a413
claytonflesher <claytonflesher@gmail.com>
2018-07-22 11:56:51 -0500

    zip traces

--- runner/app.rb
+++ runner/app.rb
@@ -1,5 +1,7 @@
-problem_directory_files = Dir.entries(File.expand_path("../problemsF", __dir__))
-problems = problem_directory_files.map do |file|
+require 'fileutils'
+require 'tmpdir'
+
+problems = Dir.entries(File.expand_path("../problemsF", __dir__)).map do |file|
   File.expand_path("../problemsF/#{file}", __dir__)
 end
 
@@ -14,4 +16,14 @@ chunked_assemblies.each do |group|
   group.map do |location, acc|
     Thread.new { puts `#{File.expand_path("../nanobots/nanobots", __dir__)} #{location}` }
   end.each { |thread| thread.join }
+end
+
+traces = Dir.entries(File.expand_path("../problemsF", __dir__)).map do |file|
+  File.expand_path("../problemsF/#{file}", __dir__)
+end.select { |file| file =~ /\.nbt/ }
+
+if SYSTEM_RUBY.contains('darwin')
+  tmp_dir = Dir.mktmpdir('traces')
+  FileUtils.cp(traces, tmp_dir)
+  `zip -r traces.zip #{tmp_dir}`
 end
